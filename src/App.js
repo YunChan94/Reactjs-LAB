009fecf9159bfd0fa707c665b18adef6db35761b
YunChan <juleefang94@gmail.com>
2022-08-20 10:23:08 +0700

    starting code lab9

--- src/App.js
+++ src/App.js
@@ -1,21 +1,8 @@
-import React, { useState, Fragment } from "react";
-import AddUser from "./Component/User/AddUser";
-import UserList from "./Component/User/UserList";
 function App() {
-  const [usersList, setUserList] = useState([]);
-  const addUserHandler = (username, userage) => {
-    setUserList((prevUserList) => {
-      return [
-        ...prevUserList,
-        { name: username, age: userage, id: Math.random().toString() },
-      ];
-    });
-  };
   return (
-    <Fragment>
-      <AddUser onAddUser={addUserHandler} />
-      <UserList users={usersList} />
-    </Fragment>
+    <div>
+      <h2>Let's get started!</h2>
+    </div>
   );
 }
 
