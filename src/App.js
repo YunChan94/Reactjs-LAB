ca370f3fff1ae335e5a3fdeacd00d0f1ee292dd5
YunChan <juleefang94@gmail.com>
2022-08-22 20:39:57 +0700

    lab10

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import React, { Fragment } from "react";
-import Header from "./components/Layout/Header";
-import Meals from "./components/Meals/Meals";
+import React, { useState, useCallback, useMemo } from "react";
+import Button from "./components/UI/Button/Button";
+import DemoOutput from "./components/Demo/DemoOutput";
+import "./App.css";
+
 function App() {
+  const [title, setTitle] = useState("My List");
+  const changeTitleHandler = useCallback(() => {
+    setTitle("New List");
+  }, []);
+  const listItems = useMemo(() => [5, 3, 1, 10, 9], []);
   return (
-    <Fragment>
-      <Header />
-      <main>
-        <Meals />
-      </main>
-    </Fragment>
+    <div className="app">
+      <DemoOutput title={title} items={listItems} />
+      <Button onClick={changeTitleHandler}>Change title</Button>
+    </div>
   );
 }
 
