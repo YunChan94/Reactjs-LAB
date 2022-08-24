797729557121f1f77fe13a242fd27af214f5dafd
YunChan <juleefang94@gmail.com>
2022-08-24 22:39:57 +0700

    add labb 11

---
+++ src/components/Users.js
@@ -0,0 +1,49 @@
+import { Component } from "react";
+import User from "./User";
+
+import classes from "./Users.module.css";
+
+const DUMMY_USERS = [
+  { id: "u1", name: "Max" },
+  { id: "u2", name: "Manuel" },
+  { id: "u3", name: "Julie" },
+];
+class Users extends Component {
+  // State lúc nào cũng là object và đặt tên là state (k đặt tên khác)
+  constructor() {
+    super();
+    this.state = { showUsers: true, more: "test" };
+  }
+
+  componentDidUpdate() {
+    if (this.props.users.length === 0) {
+      throw new Error("No users provided!");
+    }
+  }
+
+  toggleUsersHandler() {
+    //this.state.showUsers=false //NOT!
+    this.setState((curState) => {
+      return { showUsers: !curState.showUsers };
+    });
+  }
+  render() {
+    const usersList = (
+      <ul>
+        {DUMMY_USERS.map((user) => (
+          <User key={user.id} name={user.name} />
+        ))}
+      </ul>
+    );
+    return (
+      <div className={classes.users}>
+        <button onClick={this.toggleUsersHandler.bind(this)}>
+          {this.state.showUsers ? "Hide" : "Show"} Users
+        </button>
+        {this.state.showUsers && usersList}
+      </div>
+    );
+  }
+}
+
+export default Users;
