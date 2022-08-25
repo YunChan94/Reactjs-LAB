e0a703663c08d103737aafffe3158a9049dca9fc
YunChan <juleefang94@gmail.com>
2022-08-25 20:37:39 +0700

    starting code

--- src/components/User.js
+++ src/components/User.js
@@ -1,16 +1,7 @@
-import { Component } from "react";
-import classes from "./User.module.css";
-class User extends Component {
-  componentWillUnmount() {
-    console.log("user will unmount");
-  }
-  render() {
-    return <li className={classes.user}>{this.props.name}</li>;
-  }
-}
+import classes from './User.module.css';
 
-// const User = (props) => {
-//   return <li className={classes.user}>{props.name}</li>;
-// };
+const User = (props) => {
+  return <li className={classes.user}>{props.name}</li>;
+};
 
 export default User;
