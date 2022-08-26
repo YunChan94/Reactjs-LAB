9e5eb02642743ccb2cbcf01716c04362b39523a6
YunChan <juleefang94@gmail.com>
2022-08-26 22:29:37 +0700

    starting code

--- src/App.js
+++ src/App.js
@@ -1,85 +1,57 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useEffect, useState } from 'react';
 
-import MoviesList from "./components/MoviesList";
-import "./App.css";
-import AddMovie from "./components/AddMovie";
+import Tasks from './components/Tasks/Tasks';
+import NewTask from './components/NewTask/NewTask';
 
 function App() {
-  const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState("");
+  const [error, setError] = useState(null);
+  const [tasks, setTasks] = useState([]);
 
-  const fetchMoviesHandler = useCallback(async () => {
+  const fetchTasks = async (taskText) => {
+    setIsLoading(true);
     setError(null);
     try {
-      setIsLoading(true);
-      //fetch (url,{method})
       const response = await fetch(
-        "https://react-http-ce1df-default-rtdb.asia-southeast1.firebasedatabase.app/movies.json"
+        'https://react-http-6b4a6.firebaseio.com/tasks.json'
       );
 
       if (!response.ok) {
-        throw new Error("Something went wrong!⛔");
+        throw new Error('Request failed!');
       }
 
-      const data = await response.json(); // json() đổi promise thành object
-      console.log(data);
+      const data = await response.json();
 
-      const loadedMovies = [];
-      for (const key in data) {
-        loadedMovies.push({
-          id: key,
-          title: data[key].title,
-          openingText: data[key].openingText,
-          releaseDate: data[key].releaseDate,
-        });
+      const loadedTasks = [];
+
+      for (const taskKey in data) {
+        loadedTasks.push({ id: taskKey, text: data[taskKey].text });
       }
 
-      setMovies(loadedMovies);
-    } catch (error) {
-      setError(error.message);
+      setTasks(loadedTasks);
+    } catch (err) {
+      setError(err.message || 'Something went wrong!');
     }
     setIsLoading(false);
-  }, []);
+  };
 
   useEffect(() => {
-    fetchMoviesHandler();
-  }, [fetchMoviesHandler]);
+    fetchTasks();
+  }, []);
 
-  async function addMovieHandler(movie) {
-    const response = await fetch(
-      "https://react-http-ce1df-default-rtdb.asia-southeast1.firebasedatabase.app/movies.json",
-      {
-        method: "POST",
-        body: JSON.stringify(movie),
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }
-    );
-    const data = await response.json();
-    // console.log(data);
-  }
+  const taskAddHandler = (task) => {
+    setTasks((prevTasks) => prevTasks.concat(task));
+  };
 
-  let content = <p>Found no movies.</p>;
-  if (movies.length > 0) {
-    content = <MoviesList movies={movies} />;
-  }
-  if (error) {
-    content = <p>{error}</p>;
-  }
-  if (isLoading) {
-    content = <p>Loading...</p>;
-  }
   return (
     <React.Fragment>
-      <section>
-        <AddMovie onAddMovie={addMovieHandler} />
-      </section>
-      <section>
-        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
-      </section>
-      <section>{content}</section>
+      <NewTask onAddTask={taskAddHandler} />
+      <Tasks
+        items={tasks}
+        loading={isLoading}
+        error={error}
+        onFetch={fetchTasks}
+      />
     </React.Fragment>
   );
 }
