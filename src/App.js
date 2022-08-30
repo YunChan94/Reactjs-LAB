58c27bf73e8db1fad88d55a5c7284e868d73c408
YunChan94 <juleefang94@gmail.com>
2022-08-30 15:35:34 +0700

    complete validate email

--- src/App.js
+++ src/App.js
@@ -1,45 +1,10 @@
-import React, { useEffect, useState } from "react";
+import SimpleInput from './components/SimpleInput';
 
-import Tasks from "./components/Tasks/Tasks";
-import NewTask from "./components/NewTask/NewTask";
-import useHttp from "./hooks/use-http";
 function App() {
-  const [tasks, setTasks] = useState([]);
-
-  const { isLoading, error, sendRequest: fetchTasks } = useHttp();
-
-  useEffect(() => {
-    const transformTask = (taskObj) => {
-      const loadedTasks = [];
-
-      for (const taskKey in taskObj) {
-        loadedTasks.push({ id: taskKey, text: taskObj[taskKey].text });
-      }
-      setTasks(loadedTasks);
-    };
-
-    fetchTasks(
-      {
-        url: "https://lab13-custom-hook-default-rtdb.asia-southeast1.firebasedatabase.app/tasks.json",
-      },
-      transformTask
-    );
-  }, [fetchTasks]);
-
-  const taskAddHandler = (task) => {
-    setTasks((prevTasks) => prevTasks.concat(task));
-  };
-
   return (
-    <React.Fragment>
-      <NewTask onAddTask={taskAddHandler} />
-      <Tasks
-        items={tasks}
-        loading={isLoading}
-        error={error}
-        onFetch={fetchTasks}
-      />
-    </React.Fragment>
+    <div className="app">
+      <SimpleInput />
+    </div>
   );
 }
 
---
+++ src/components/SimpleInput.js
@@ -0,0 +1,100 @@
+import { useState } from "react";
+
+const SimpleInput = (props) => {
+  // Name
+  const [enteredName, setEnteredName] = useState("");
+  const [enteredNameTouch, setEnteredNameTouch] = useState(false);
+  // Email
+  const [enteredEmail, setEnteredEmail] = useState("");
+  const [enteredEmailTouch, setEnteredEmailTouch] = useState(false);
+
+  const enteredEmailIsValid =
+    enteredEmail.trim() !== "" || enteredEmail.includes("@");
+  const emailInputIsInvalid = !enteredEmailIsValid && enteredEmailTouch;
+
+  const enteredNameIsValid = enteredName.trim() !== "";
+  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouch;
+
+  let formIsValid = false;
+  if (enteredNameIsValid && enteredEmailIsValid) {
+    formIsValid = true;
+  }
+
+  const nameInputChangeHandler = (event) => {
+    setEnteredName(event.target.value);
+  };
+
+  const namInputBlurHandler = (event) => {
+    setEnteredNameTouch(true);
+  };
+
+  const formSubmissionHandler = (event) => {
+    event.preventDefault();
+
+    if (!formIsValid) {
+      return;
+    }
+    // setEnteredNameTouch(true);
+
+    console.log(enteredName, enteredEmail);
+    setEnteredName("");
+    setEnteredEmail("");
+    setEnteredNameTouch(false);
+    setEnteredEmailTouch(false);
+  };
+
+  ///// VALIDATE EMAIL
+
+  const emailInputChangeHandler = (event) => {
+    setEnteredEmail(event.target.value);
+  };
+  const emailInputBlurHandler = (event) => {
+    setEnteredEmailTouch(true);
+  };
+
+  const nameInputClasses = nameInputIsInvalid
+    ? "form-control invalid"
+    : "form-control";
+
+  const emailInputClasses = emailInputIsInvalid
+    ? "form-control invalid"
+    : "form-control";
+  return (
+    <form onSubmit={formSubmissionHandler}>
+      <div className={nameInputClasses}>
+        <label htmlFor="name">Your Name</label>
+        <input
+          type="text"
+          id="name"
+          onChange={nameInputChangeHandler}
+          onBlur={namInputBlurHandler}
+          value={enteredName}
+        />
+        {nameInputIsInvalid && (
+          <p className="error-text">Name must not be empty.</p>
+        )}
+      </div>
+      <div className={emailInputClasses}>
+        <label htmlFor="email">E-Mail</label>
+        <input
+          type="text"
+          id="name"
+          onChange={emailInputChangeHandler}
+          onBlur={emailInputBlurHandler}
+          value={enteredEmail}
+        />
+
+        {emailInputIsInvalid && (
+          <p className="error-text">
+            Email must not be empty and includes "@".
+          </p>
+        )}
+      </div>
+      <div className="form-actions">
+        <button disabled={!formIsValid}>Submit</button>
+      </div>
+    </form>
+  );
+};
+
+export default SimpleInput;
